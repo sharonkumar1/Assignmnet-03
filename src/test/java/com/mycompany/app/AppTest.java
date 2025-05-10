56b8ca99c1be0822233e47ad8694471856c8235b
sharonkumar1 <ksharonkumar01@gmail.com>
2025-05-10 07:40:04 +0530

    Reorganize: flatten project into repo root

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);  // Simple test to ensure everything is working
+    }
+}
